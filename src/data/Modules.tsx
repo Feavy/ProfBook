b7ef575482fd0d7d253110a24cbb236913a4b54e
Feavy <quentin071100@live.fr>
2019-06-11 21:57:04 +0200

    Created build

--- src/data/Modules.tsx
+++
@@ -1,1 +0,0 @@
-export default ['IAP', 'ISI', 'IBD', 'ANI', 'SDA', 'FC', 'PPP', 'CDIN', 'EE', 'EG', 'AL', 'MD', 'IHM', 'AMN', 'GLA', 'RES', 'POO', 'CIA', 'PABD', 'COO', 'APR', 'EGD', 'GP', 'SC'].sort();
