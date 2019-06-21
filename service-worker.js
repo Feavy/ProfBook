1afd2f84cbc0f777c22ab2f2c8cd01d4b4a218b7
Feavy <quentin071100@live.fr>
2019-06-21 18:36:31 +0200

    Profbook v2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1756a441f53f50e3b621060c9346346d.js"
+  "/Feavy/ProfBook/precache-manifest.d60c6f559a67a4bbf9c5a926cfd056a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Feavy/ProfBook/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
